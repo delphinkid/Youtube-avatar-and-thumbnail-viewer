79f0cab9c8920a501ec0049fc96859cb64bc5ff6
delphinkid <55378193+delphinkid@users.noreply.github.com>
2020-01-31 22:25:42 -0600

    Update Youtube_avatar_and_thumbnail_viewer.user.js

--- Youtube_avatar_and_thumbnail_viewer.user.js
+++ Youtube_avatar_and_thumbnail_viewer.user.js
@@ -22,6 +22,7 @@ function initMenu(aEvent) {
   var node = aEvent.target;
   var item = document.querySelector("#avatar-expand menuitem");
   item.setAttribute("imageURL", node.src);
+  item.setAttribute("thumbURL", node.parentElement.parentElement.parentElement.getElementsByTagName("yt-img-shadow")[0].getElementsByTagName("img")[0].src);
 }
 function imgPlace() {
   var img = document.getElementsByTagName("img");
@@ -32,20 +33,24 @@ function imgPlace() {
   }
 };
 function openImage(aEvent) {
-  var imageURL = aEvent.target.getAttribute("imageURL");
-  var replacer = /([\/\=])(s)\d{2,}-/;
-  var coverReplacer = /(=)(w\d\d-h\d\d\d)-/
+  var imageURL = aEvent.target.getAttribute("imageurl");
+  var thumbURL = aEvent.target.getAttribute("thumbURL");
+  var replacer = /([\/\=])(s)\d{2,}/;
+  var coverReplacer = /(=)(w\d\d-h\d\d\d)/
   var slicer = /\?sqp=/;
+  var slicer2 = /an_webp\/([^\/]*)\//;
   var maxres = /hqdefault/;
   if (replacer.exec(imageURL) != null) {
-    var bigURL = imageURL.replace(replacer, replacer.exec(imageURL)[1] + 'w500-');
+    var bigURL = imageURL.replace(replacer, replacer.exec(imageURL)[1] + 'w500');
   }
   if (coverReplacer.exec(imageURL) != null) {
     var bigURL = imageURL.replace(coverReplacer, coverReplacer.exec(imageURL)[1] + 'w500-h500-');
   }
   if (slicer.exec(imageURL) != null) {
     var bigURL = imageURL.split(slicer)[0];
-    //var bigURL = bigURL.replace(maxres, 'maxresdefault');
+  }
+	else if (slicer.exec(thumbURL) != null) {
+    var bigURL = thumbURL.split(slicer)[0];
   }
   window.open(bigURL);
 }
